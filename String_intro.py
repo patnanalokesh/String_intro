3a43c5fa2eb18d4ee04838d4b893cf245496fffd
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-27 18:51:38 +0530

    Add files via upload

---
+++ String_intro.py
@@ -0,0 +1,18 @@
+a='department'
+# print(a,type(a))
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+# print(a[6])
+# print(a[7])
+# print(a[8])
+# print(a[9])
+# print(a[10])#IndexError: string index out of range
+# print(a[0]+a[1]+a[2]+a[3]+a[4]+a[5])
+# print(a[3]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9])
+# print(a[0:6])
+# print(a[3]+a[2:])
+# print(a[3]+a[2:10])
